aceae561880486b98365e7050efe01982553417a
lephuongtrang45678 <lephuongtrang45678@gmai.com>
2021-11-01 19:43:32 +0700

    danhba

---
+++ class.php
@@ -0,0 +1,19 @@
+<?php
+    //dinh nghia lop
+    class personal{
+    //thuoc tinh
+        private $manv;
+        private $tennv;
+        private$chucvu;
+        private$sodidong;
+        private$email;
+        private$tendv;
+
+        //phuong thuc(ham)
+        //ham tao: khoi tao gia tri 
+        public function __construct($umanv)
+        {
+        $this->manv = NULL;
+        $this->tennv = null;
+        }
+    }
--- import.php
+++ import.php
@@ -1,60 +1,71 @@
 <?php  
+include("./CONFIG/constants.php");
     if($_FILES["upload-nhanvien"]["name"])
     { 
-        $connect = mysqli_connect("localhost", "root", "", "danhba");  
+        // $conn = mysqli_conn("localhost", "root", "", "danhba");  
         $output = '';  
         $allowed_ext = array("csv");  
-        $extension = end(explode(".", $_FILES["upload-nhanvien"]["name"]));  
-        if(in_array($extension, $allowed_ext))  
+        $extension = explode(".", $_FILES["upload-nhanvien"]["name"]);  
+        $end_ex =end($extension);
+        if(in_array($end_ex, $allowed_ext))  
         {  
             $file_data = fopen($_FILES["upload-nhanvien"]["tmp_name"], 'r');  
             fgetcsv($file_data);  
             while($row = fgetcsv($file_data))  
             {
-                    $tennv = mysqli_real_escape_string($connect, $row[0]);  
-                    $chucvu = mysqli_real_escape_string($connect, $row[1]);  
-                    $sodidong = mysqli_real_escape_string($connect, $row[2]);  
-                    $email = mysqli_real_escape_string($connect, $row[3]);  
-                    $madv = mysqli_real_escape_string($connect, $row[4]);  
+                
+                    $tennv = mysqli_real_escape_string($conn, $row[0]);  
+                    $chucvu = mysqli_real_escape_string($conn, $row[1]);  
+                    $sodidong = mysqli_real_escape_string($conn, $row[2]);  
+                    $email = mysqli_real_escape_string($conn, $row[3]);  
+                    $madv = mysqli_real_escape_string($conn, $row[4]);  
                     $query = "  
                     INSERT INTO db_nhanvien  
                         (tennv, chucvu, email, sodidong, madv)  
                         VALUES ('$tennv', '$chucvu', '$email', '$sodidong', '$madv')  
                     ";  
-                    mysqli_query($connect, $query);  
+                    mysqli_query($conn, $query);  
+                    echo $query;
             }  
-            $select = "SELECT * FROM db_nhanvien  ORDER BY manv DESC";  
-            $result = mysqli_query($connect, $select);  
-            $output .= '  
-                    <table class="table table-bordered">  
-                        <tr>  
-                            <th width="5%">Mã nhân viên</th>  
-                            <th width="25%">Họ và tên</th>  
-                            <th width="35%">Chức vụ</th>  
-                            <th width="10%">Số di động</th>  
-                            <th width="20%">Email</th>  
-                            <th width="5%">Tên đơn vị</th>  
-                        </tr>  
-            ';  
-            while($row = mysqli_fetch_array($result))  
-            {  
-                    $output .= '  
-                        <tr>  
-                            <td>'.$row["manv"].'</td>  
-                            <td>'.$row["tennv"].'</td>  
-                            <td>'.$row["chucvu"].'</td>  
-                            <td>'.$row["email"].'</td>  
-                            <td>'.$row["sodidong"].'</td>  
-                            <td>'.$row["madv"].'</td>  
-                        </tr>  
-                    ';  
-            }  
-            $output .= '</table>';  
-            echo $output;  
-        }  
-        else  
-        {  
-            echo 'Error1';  
+        //     $select = "SELECT * FROM db_nhanvien  ORDER BY manv DESC";  
+        //     $result = mysqli_query($conn, $select);  
+        //     $output .= '  
+        //             <table class="table table-bordered">  
+        //                 <tr>
+        //                 <th scope="col">STT</th>
+        //                 <th scope="col">Mã nhân viên</th>
+        //                 <th scope="col">Họ và tên</th>
+        //                 <th scope="col">Chức vụ</th>
+        //                 <th scope="col">Số di động</th>
+        //                 <th scope="col">Email</th>
+        //                 <th scope="col">Tên đơn vị</th>
+        //                 <th scope="col">Sửa</th>
+        //                 <th scope="col">Xóa</th>
+        //                 <th scope="col" style="width: 10%;">Ảnh đại diện</th>
+
+        //             </tr> 
+        //     ';  
+        //     while($row = mysqli_fetch_array($result))  
+        //     {
+        //         $i = 1;
+        //             $output .= '  
+        //                 <tr>  
+        //                     <td>' . $i . '</td>  
+        //                     <td>'.$row["manv"].'</td>  
+        //                     <td>'.$row["tennv"].'</td>  
+        //                     <td>'.$row["chucvu"].'</td>  
+        //                     <td>'.$row["sodidong"].'</td>  
+        //                     <td>'.$row["email"].'</td>  
+        //                     <td>'.$row["madv"].'</td>  
+        //                 </tr>  
+        //             ';  
+        //     }  
+        //     $output .= '</table>';  
+        //     echo $output;  
+        // }  
+        // else  
+        // {  
+        //     echo 'Error1';  
         } 
     }
     else  
